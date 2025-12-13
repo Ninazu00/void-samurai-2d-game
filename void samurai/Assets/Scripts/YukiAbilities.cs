15f09dfaf0bfce638e4820c82bea05629e6ec4c8
Ninazu00 <ba.mohammad2005@gmail.com>
2025-12-13 21:33:26 +0200

    Added VoidProjectiles

--- void samurai/Assets/Scripts/YukiAbilities.cs
+++ void samurai/Assets/Scripts/YukiAbilities.cs
@@ -6,11 +6,17 @@ public class YukiAbilities : MonoBehaviour
 {
 
     public Transform swords;
-    public Transform[] spawnPoints;
+    public Transform[] swordSpawnPoints;
     public SpriteRenderer LspriteRenderer;
     public Sprite LnewSprite;  
     public SpriteRenderer RspriteRenderer;
-    public Sprite RnewSprite;  
+    public Sprite RnewSprite;
+    public Transform voidProjectile;
+    public Transform spawnNovaDown;
+    public Transform spawnNovaLeft;
+    public Transform spawnNovaRight;
+    public Transform spawnNovaDL;
+    public Transform spawnNovaDR;
     // Start is called before the first frame update
     void Start()
     {
@@ -30,11 +36,49 @@ public class YukiAbilities : MonoBehaviour
     }
     public void spawnSwords()
     {
-        foreach (Transform sp in spawnPoints)
+        foreach (Transform sp in swordSpawnPoints)
             {
                 float randomZ = Random.Range(-20f, 20f);
                 Quaternion randomRotation = Quaternion.Euler(0, 0, randomZ);
                 Instantiate(swords, sp.position, randomRotation);
             }
+    }
+    public void spawnVoidBurst()
+    {
+        
+        Instantiate(voidProjectile, spawnNovaDown.position, spawnNovaDown.transform.rotation);
+        Instantiate(voidProjectile, spawnNovaLeft.position, spawnNovaLeft.transform.rotation);
+        Instantiate(voidProjectile, spawnNovaRight.position, spawnNovaRight.transform.rotation);
+        for (int i = 0; i < 3; i++)
+        {
+            float randomZ = Random.Range(-70f, -120f);
+            Quaternion randomRotation = Quaternion.Euler(0, 0, randomZ);
+            Instantiate(voidProjectile, spawnNovaLeft.position, randomRotation);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            float randomZ = Random.Range(70f, 120f);
+            Quaternion randomRotation = Quaternion.Euler(0, 0, randomZ);
+            Instantiate(voidProjectile, spawnNovaRight.position, randomRotation);
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            float randomZ = Random.Range(-45f, 45f);
+            Quaternion randomRotation = Quaternion.Euler(0, 0, randomZ);
+            Instantiate(voidProjectile, spawnNovaDown.position, randomRotation);
+        }
+        for (int i = 0; i < 50; i+=20)
+        {
+            float randomZ = Random.Range(-20f-i, -40f-i);
+            Quaternion randomRotation = Quaternion.Euler(0, 0, randomZ);
+            Instantiate(voidProjectile, spawnNovaDL.position, randomRotation);
+        }
+        for (int i = 0; i < 50; i+=20)
+        {
+            float randomZ = Random.Range(20f+i, 40f+i);
+            Quaternion randomRotation = Quaternion.Euler(0, 0, randomZ);
+            Instantiate(voidProjectile, spawnNovaDR.position, randomRotation);
+        }
+
     }
 }
--- void samurai/Assets/Temporary/TestingSpawnTrigger.cs
+++ void samurai/Assets/Temporary/TestingSpawnTrigger.cs
@@ -4,23 +4,19 @@ using UnityEngine;
 
 public class TestingSpawnTrigger : MonoBehaviour
 {
-    public Transform swords;
-    public Transform[] spawnPoints;
-    public SpriteRenderer LspriteRenderer;
-    public Sprite LnewSprite;  
-    public SpriteRenderer RspriteRenderer;
-    public Sprite RnewSprite;  
     // Start is called before the first frame update
     void Start()
     {
         
     }
+
     // Update is called once per frame
     void OnTriggerEnter2D(Collider2D other){
         if(other.tag == "Player")
         {
             FindObjectOfType<YukiAbilities>().updateImages();
             FindObjectOfType<YukiAbilities>().spawnSwords();
+            FindObjectOfType<YukiAbilities>().spawnVoidBurst();
         }
     }
 }
