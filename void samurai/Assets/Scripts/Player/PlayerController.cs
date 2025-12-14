b6f400810bd0b9ba138417daceed5dd18689e903
RamyAI <ramy242781@bue.edu.eg>
2025-12-15 01:04:43 +0200

    Player Controller Animation And Light,Heavy Slash Animation

--- void samurai/Assets/Scripts/Player/PlayerController.cs
+++ void samurai/Assets/Scripts/Player/PlayerController.cs
@@ -6,64 +6,101 @@ public class PlayerController : MonoBehaviour {
 
     public float moveSpeed; //how fast the character moves
     public float jumpHeight; //how high the character jumps
-    //private bool IsFacingRight; //check if player is facing right
-    public KeyCode Spacebar; //Jump is the name we gave a keyboard button we chose to be the jump button. In this case, 
-    //we chose the Space button, and called it Spacebar. To allocate the Space button to the name Spacebar, go to the Script 
-    //component of your player character, and choose Space from the dropdown list
-    public KeyCode L;//L is the name we gave a keyboard button we chose to be the left movement button.
-    public KeyCode R;//R is the name we gave a keyboard button we chose to be the right movement button.
-    public Transform groundCheck; //an invisible point in space. We use it to see if the player is touching the ground
-    public float groundCheckRadius; //a value to determine how big the circle around the player's feet is, and therefore determine how
-    //close he is to the ground
-    public LayerMask whatIsGround; //this variable stores what is considered a ground to the character
-    private bool grounded; //check if the character is standing on solid ground;
-    // Use this for initialization
-    private Animator anim;
-	void Start () {
-        anim =GetComponent<Animator>();
-    }
-	// Update is called once per frame
-	void Update () {
-        //anim.SetFloat("Speed",Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x));
-        //anim.SetFloat("Height", GetComponent<Rigidbody2D>().velocity.y);
-        //anim.SetBool("Ground", grounded);
-        if(Input.GetKeyDown(Spacebar) && grounded) //When user presses the space button ONCE
-        {
-            Jump(); //see function definition below   
-        }
-       
-        if (Input.GetKey(L)) //When user presses the left arrow button
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-moveSpeed, GetComponent<Rigidbody2D>().velocity.y); 
-            //player character moves horizontally to the left along the x-axis without disrupting jump
+    public KeyCode Spacebar; //Jump key
+    public KeyCode L; //Left movement key
+    public KeyCode R; //Right movement key
+    public KeyCode LightAttackKey; //Light attack key
+    public KeyCode HeavyAttackKey; //Heavy attack key
+    public Transform groundCheck; //ground check position
+    public float groundCheckRadius; //ground check radius
+    public LayerMask whatIsGround; //what is considered ground
 
-            if(GetComponent<SpriteRenderer>()!=null)
+    private bool grounded; //is player on ground
+    private Animator anim;
+    private Rigidbody2D rb; //reference to Rigidbody2D for velocity access
+
+    void Start () {
+        anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>(); //get Rigidbody2D component
+    }
+
+    void Update () {
+
+        // Jump
+        if(Input.GetKeyDown(Spacebar) && grounded)
+        {
+            Jump();  
+        }
+
+        // Move Left
+        if (Input.GetKey(L))
+        {
+            rb.velocity =
+                new Vector2(-moveSpeed, rb.velocity.y);
+
+            if(GetComponent<SpriteRenderer>() != null)
             {
                 GetComponent<SpriteRenderer>().flipX = true;
-            }           
+            }
         }
-      
-        if (Input.GetKey(R)) //When user presses the left arrow button
-        {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(moveSpeed, GetComponent<Rigidbody2D>().velocity.y); 
-            //player character moves horizontally to the right along the x-axis without disrupting jump
 
-            if(GetComponent<SpriteRenderer>()!=null)
+        // Move Right
+        if (Input.GetKey(R))
+        {
+            rb.velocity =
+                new Vector2(moveSpeed, rb.velocity.y);
+
+            if(GetComponent<SpriteRenderer>() != null)
             {
                 GetComponent<SpriteRenderer>().flipX = false;
-            }   
+            }
+        }
+
+        // Stop sliding when no movement key is pressed
+        if (!Input.GetKey(L) && !Input.GetKey(R))
+        {
+            rb.velocity =
+                new Vector2(0f, rb.velocity.y);
+        }
+
+        // ----------- ANIMATION (Idle <-> Run) -----------
+        if (Input.GetKey(L) || Input.GetKey(R))
+        {
+            anim.SetBool("isRunning", true);
+        }
+        else
+        {
+            anim.SetBool("isRunning", false);
+        }
+
+        // ----------- ANIMATION (Jump & Fall Blend Tree) -----------
+        anim.SetFloat("yVelocity", rb.velocity.y); //send Y velocity to blend tree
+        anim.SetBool("isGrounded", grounded); //tell animator if player is on ground
+
+        // ----------- ANIMATION (Light & Heavy Attacks) -----------
+        if (Input.GetKeyDown(LightAttackKey))
+        {
+            anim.SetTrigger("lightAttack"); //trigger light slash animation
+        }
+
+        if (Input.GetKeyDown(HeavyAttackKey))
+        {
+            anim.SetTrigger("heavyAttack"); //trigger heavy slash animation
         }
     }
 
     void FixedUpdate()
     {
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, whatIsGround); //this statement calculates when 
-        //exactly the character is considered by Unity's engine to be standing on the ground. 
+        grounded = Physics2D.OverlapCircle(
+            groundCheck.position,
+            groundCheckRadius,
+            whatIsGround
+        ); //this statement calculates when exactly the character is considered to be standing on the ground
     }
 
     void Jump()
     {
-        GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight); //player character jumps 
-        //vertically along the y-axis without disrupting horizontal walk      
+        rb.velocity =
+            new Vector2(rb.velocity.x, jumpHeight); //player character jumps vertically along the y-axis without disrupting horizontal walk
     }
 }
