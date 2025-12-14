0881befec68f3000d74627bedb43647158cbf041
Eigengrau53 <IForgorskl53@outlook.com>
2025-12-14 18:07:20 +0200

    More level 3 work

--- void samurai/Assets/Scripts/VoidSpikes.cs
+++ void samurai/Assets/Scripts/VoidSpikes.cs
@@ -1,8 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-/*
-public class Falldeath : MonoBehaviour
+
+public class VoidSpikes : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
@@ -23,4 +23,3 @@ public class Falldeath : MonoBehaviour
         
     }
 }
-*/
