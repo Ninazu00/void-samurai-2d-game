a0da5ae6e8d68f2aac25339229d16356c65d7886
xDrunk1 <yousef245737@bue.edu.eg>
2025-12-15 18:30:47 +0200

    Blacksmith

--- void samurai/Assets/Scripts/BlacksmithNPC.cs
+++ void samurai/Assets/Scripts/BlacksmithNPC.cs
@@ -5,14 +5,16 @@ using UnityEngine;
 public class BlacksmithNPC : MonoBehaviour
 {
     public float interactionRange = 2f;
-    public int healthUpgrade = 20;
-    public int levelNumber = 1;
+    public int requiredVoidsteelOre = 3; // Require 3 ore per upgrade
+    public int lightDamageIncrease = 2; // Increase light attack by 2
+    public int heavyDamageIncrease = 5; // Increase heavy attack by 5
+    public int healthIncrease = 25; // Increase health by 25
+    public int maxUpgrades = 3; // Maximum of 3 upgrades allowed
+    
     private Transform player;
     private PlayerStats playerStats;
-    private bool playerInRange = false;
-    private bool hasUpgraded = false;
-    
-    public static int checkpointsVisited = 0;
+    private PlayerController playerController;
+    private int upgradesUsed = 0; // Track number of upgrades already used
 
     void Start()
     {
@@ -21,42 +23,70 @@ public class BlacksmithNPC : MonoBehaviour
         {
             player = playerObj.transform;
             playerStats = player.GetComponent<PlayerStats>();
+            playerController = player.GetComponent<PlayerController>();
         }
-        gameObject.SetActive(false);
+        
+        // Blacksmith is always active (normal NPC)
+        gameObject.SetActive(true);
     }
 
     void Update()
     {
         if (player == null) return;
-
-        if (!gameObject.activeSelf)
-        {
-            if (checkpointsVisited >= levelNumber)
-            {
-                gameObject.SetActive(true);
-            }
-            return;
-        }
         
+        // Check if player is in interaction range
         float dist = Vector2.Distance(transform.position, player.position);
-        playerInRange = dist <= interactionRange;
         
-        if (playerInRange && Input.GetKeyDown(KeyCode.E))
+        // Check all upgrade conditions
+        if (dist <= interactionRange && Input.GetKeyDown(KeyCode.E))
         {
-            Upgrade();
+            if (upgradesUsed >= maxUpgrades)
+            {
+                Debug.Log($"Maximum upgrades ({maxUpgrades}) already used!");
+            }
+            else if (PlayerStats.score < requiredVoidsteelOre)
+            {
+                Debug.Log($"Need {requiredVoidsteelOre - PlayerStats.score} more Voidsteel Ore to upgrade!");
+            }
+            else
+            {
+                Upgrade();
+            }
         }
     }
+
     void Upgrade()
     {
-        if (playerStats != null && !hasUpgraded)
+        if (playerStats != null && playerController != null && upgradesUsed < maxUpgrades)
         {
-            playerStats.health += healthUpgrade;
-            hasUpgraded = true;
-            Debug.Log("Upgraded! New health: " + playerStats.health);
-        }
-        else if (hasUpgraded)
-        {
-            Debug.Log("Already upgraded in this level!");
+            // Deduct the ore cost
+            PlayerStats.score -= requiredVoidsteelOre;
+            
+            // Increase health
+            playerStats.health += healthIncrease;
+            
+            // Update health slider UI
+            if (playerStats.slider != null)
+            {
+                playerStats.slider.maxValue = playerStats.health;
+                playerStats.slider.value = playerStats.health;
+            }
+            
+            // Increase attack damage
+            playerController.lightDamage += lightDamageIncrease;
+            playerController.heavyDamage += heavyDamageIncrease;
+            
+            upgradesUsed++;
+            
+            Debug.Log($"Upgraded! Health: {playerStats.health}, Light Damage: {playerController.lightDamage}, Heavy Damage: {playerController.heavyDamage}");
+            Debug.Log($"Upgrades remaining: {maxUpgrades - upgradesUsed}");
+            
+            // Notify when all upgrades are used
+            if (upgradesUsed >= maxUpgrades)
+            {
+                Debug.Log("All upgrades have been used!");
+            }
         }
     }
 }
+
